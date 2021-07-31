b246c3b2e8b42b10e3bff60d745d4c12eeee73bb
Huy Duong <huy.duongdinh@gmail.com>
2021-07-31 18:23:30 +0700

    Disable unused builtin vim plugins + install packer.nvim

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,3 +1,31 @@
+-- Disable builtin vim plugins
+local disabled_built_ins = {
+    "netrw",
+    "netrwPlugin",
+    "netrwSettings",
+    "netrwFileHandlers",
+    "gzip",
+    "zip",
+    "zipPlugin",
+    "tar",
+    "tarPlugin",
+    "getscript",
+    "getscriptPlugin",
+    "vimball",
+    "vimballPlugin",
+    "2html_plugin",
+    "logipat",
+    "rrhelper",
+    "spellfile_plugin",
+    "matchit"
+}
+
+for _, plugin in pairs(disabled_built_ins) do
+    vim.g["loaded_" .. plugin] = 1
+end
+
+local plugin_loader = require("plugins.plugin-loader").init()
+
 require 'general.settings'
 require 'general.mappings'
 
