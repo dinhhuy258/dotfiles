6447b7a8d7aa440af624d22c1dca93d5f4302331
Huy Duong <huy.duongdinh@gmail.com>
2021-08-01 14:30:16 +0700

    Refactor + add json lsp

--- nvim/lua/default-config.lua
+++ nvim/lua/default-config.lua
@@ -1,95 +1,8 @@
-DATA_PATH = vim.fn.stdpath "data"
-CACHE_PATH = vim.fn.stdpath "cache"
-TERMINAL = vim.fn.expand "$TERMINAL"
-USER = vim.fn.expand "$USER"
-
 nvim = {
-  leader = "space",
-  colorscheme = "spacegray",
-  line_wrap_cursor_movement = true,
-  transparent_window = false,
-  format_on_save = true,
-  vsnip_dir = os.getenv "HOME" .. "/.config/snippets",
-  database = { save_location = "~/.config/lunarvim_db", auto_execute = 1 },
-  keys = {},
-
-  builtin = {
-    lspinstall = {},
-    telescope = {},
-    compe = {},
-    autopairs = {},
-    treesitter = {},
-    nvimtree = {},
-    gitsigns = {},
-    which_key = {},
-    comment = {},
-    rooter = {},
-    galaxyline = {},
-    bufferline = {},
-    dap = {},
-    dashboard = {},
-    terminal = {},
-  },
-
   lsp = {
-    completion = {
-      item_kind = {
-        "   (Text) ",
-        "   (Method)",
-        "   (Function)",
-        "   (Constructor)",
-        " ﴲ  (Field)",
-        "[] (Variable)",
-        "   (Class)",
-        " ﰮ  (Interface)",
-        "   (Module)",
-        " 襁 (Property)",
-        "   (Unit)",
-        "   (Value)",
-        " 練 (Enum)",
-        "   (Keyword)",
-        "   (Snippet)",
-        "   (Color)",
-        "   (File)",
-        "   (Reference)",
-        "   (Folder)",
-        "   (EnumMember)",
-        " ﲀ  (Constant)",
-        " ﳤ  (Struct)",
-        "   (Event)",
-        "   (Operator)",
-        "   (TypeParameter)",
-      },
-    },
-    diagnostics = {
-      signs = {
-        active = true,
-        values = {
-          { name = "LspDiagnosticsSignError", text = "" },
-          { name = "LspDiagnosticsSignWarning", text = "" },
-          { name = "LspDiagnosticsSignHint", text = "" },
-          { name = "LspDiagnosticsSignInformation", text = "" },
-        },
-      },
-      virtual_text = {
-        prefix = "",
-        spacing = 0,
-      },
-      underline = true,
-      severity_sort = true,
-    },
-    override = {},
-    popup_border = "single",
     on_attach_callback = nil,
     on_init_callback = nil,
   },
-
-  plugins = {
-    -- use config.lua for this not put here
-  },
-
-  autocommands = {},
-  debug = false,
 }
 
 local schemas = nil
@@ -107,7 +20,7 @@ nvim.lang = {
     formatters = {
       {
         -- @usage can be gofmt or goimports or gofumpt
-        exe = "",
+        exe = "gofmt",
         args = {},
         stdin = true,
       },
@@ -125,15 +38,47 @@ nvim.lang = {
       },
     },
   },
+  json = {
+    formatters = {
+      {
+        -- @usage can be json_tool or prettier or prettierd
+        exe = "",
+        args = {},
+        stdin = true,
+      },
+    },
+    linters = {},
+    lsp = {
+      provider = "jsonls",
+      setup = {
+        cmd = {
+          "node",
+          DATA_PATH .. "/lspinstall/json/vscode-json/json-language-features/server/dist/node/jsonServerMain.js",
+          "--stdio",
+        },
+        on_attach = common_on_attach,
+        on_init = common_on_init,
+        capabilities = common_capabilities,
+        settings = {
+          json = {
+            schemas = schemas,
+            --   = {
+            --   {
+            --     fileMatch = { "package.json" },
+            --     url = "https://json.schemastore.org/package.json",
+            --   },
+            -- },
+          },
+        },
+        commands = {
+          Format = {
+            function()
+              vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line "$", 0 })
+            end,
+          },
+        },
+      },
+    },
+  },
 }
 
--- require("core.which-key").config()
--- require "core.status_colors"
--- require("core.gitsigns").config()
--- require("core.compe").config()
--- require("core.dashboard").config()
--- require("core.dap").config()
--- require("core.terminal").config()
--- require("core.telescope").config()
--- require("core.treesitter").config()
--- require("core.nvimtree").config()
