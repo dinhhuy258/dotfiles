e3cc5970a8285683a5714d58b6b97914fbd08027
Huy Duong <huy.duongdinh@gmail.com>
2021-08-01 11:17:18 +0700

    Fix nvim-autopairs

--- nvim/lua/plugins/nvim-autopairs.lua
+++ nvim/lua/plugins/nvim-autopairs.lua
@@ -27,7 +27,7 @@ M.setup = function()
 
   if package.loaded["compe"] then
     require("nvim-autopairs.completion.compe").setup {
-  ;   map_cr = true, --  map <CR> on insert mode
+      map_cr = true, --  map <CR> on insert mode
       map_complete = true, -- it will auto insert `(` after select function or method item
     }
   end
