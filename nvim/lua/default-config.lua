64146e2e68c6db8bc368fc69bb630b1c4255e993
Huy Duong <huy.duongdinh@gmail.com>
2021-08-01 15:09:46 +0700

    Lua lsp config

--- nvim/lua/default-config.lua
+++ nvim/lua/default-config.lua
@@ -39,6 +39,53 @@ nvim.lang = {
       },
     },
   },
+  lua = {
+    formatters = {
+      {
+        -- @usage can be stylua or lua_format
+        exe = "stylua",
+        args = {},
+      },
+    },
+    linters = {},
+    lsp = {
+      provider = "sumneko_lua",
+      setup = {
+        cmd = {
+          DATA_PATH .. "/lspinstall/lua/sumneko-lua-language-server",
+          "-E",
+          DATA_PATH .. "/lspinstall/lua/main.lua",
+        },
+        capabilities = common_capabilities,
+        on_attach = common_on_attach,
+        on_init = common_on_init,
+        settings = {
+          Lua = {
+            runtime = {
+              -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+              version = "LuaJIT",
+              -- Setup your lua path
+              path = vim.split(package.path, ";"),
+            },
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = { "vim" },
+            },
+            workspace = {
+              -- Make the server aware of Neovim runtime files
+              library = {
+                [vim.fn.expand "~/.config/nvim/lua"] = true,
+                [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+                [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+              },
+              maxPreload = 100000,
+              preloadFileSize = 1000,
+            },
+          },
+        },
+      },
+    },
+  },
   json = {
     formatters = {
       {
