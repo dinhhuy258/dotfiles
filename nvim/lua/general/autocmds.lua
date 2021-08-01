1f40fea39dd70c0ce09942c2f06563a1e19a0ea4
Huy Duong <huy.duongdinh@gmail.com>
2021-08-01 14:53:23 +0700

    Add autocmds file

---
+++ nvim/lua/general/autocmds.lua
@@ -0,0 +1,58 @@
+local M = {}
+
+local default_autogroups = {
+  _general_settings = {
+    -- Stop newline continution of comments
+    {
+      "FileType",
+      "*",
+      "setlocal formatoptions-=c formatoptions-=r formatoptions-=o",
+    },
+    -- When a file has been detected to have been changed outside of Vim and it has not been changed inside of Vim, automatically read it again
+    {
+      "FocusGained, BufEnter",
+      "*",
+      ":checktime",
+    },
+    -- Highlighting yarked region
+    {
+      "TextYankPost",
+      "*",
+      "lua require('vim.highlight').on_yank({ higroup = 'HighlightedYankRegion', timeout = 1000 })",
+    },
+  },
+  _packer_compile = {
+    { "BufWritePost", "plugins.lua", "PackerCompile" },
+  },
+  _general_lsp = {
+    { "FileType", "lspinfo", "nnoremap <silent> <buffer> q :q<CR>" },
+  },
+}
+
+function M.define_default_autogroups()
+  M.define_augroups(default_autogroups)
+end
+
+function M.define_augroups(definitions)
+  -- Create autocommand groups based on the passed definitions
+  --
+  -- The key will be the name of the group, and each definition
+  -- within the group should have:
+  --    1. Trigger
+  --    2. Pattern
+  --    3. Text
+  -- just like how they would normally be defined from Vim itself
+  for group_name, definition in pairs(definitions) do
+    vim.cmd("augroup " .. group_name)
+    vim.cmd "autocmd!"
+
+    for _, def in pairs(definition) do
+      local command = table.concat(vim.tbl_flatten { "autocmd", def }, " ")
+      vim.cmd(command)
+    end
+
+    vim.cmd "augroup END"
+  end
+end
+
+return M
