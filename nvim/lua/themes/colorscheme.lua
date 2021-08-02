96f038ca033dcc915def01778e53e5550ff3c57a
Huy Duong <huy.duongdinh@gmail.com>
2021-08-02 22:30:29 +0700

    Update Color scheme

--- nvim/lua/themes/colorscheme.lua
+++ nvim/lua/themes/colorscheme.lua
@@ -4,11 +4,13 @@ vim.cmd('set termguicolors')
 -- Tell vim what the background color looks like
 vim.o.background = 'dark'
 
-vim.cmd('highlight clear SignColumn')
-vim.cmd('highlight Normal guibg=NONE ctermbg=NONE')
-vim.cmd('highlight EndOfBuffer guibg=NONE ctermbg=NONE')
-vim.cmd('highlight NonText guibg=NONE ctermbg=NONE')
-vim.cmd('highlight CursorLineNR guibg=NONE ctermbg=NONE')
-vim.cmd('highlight Comment cterm=italic')
 vim.cmd('highlight HighlightedYankRegion cterm=reverse gui=reverse')
+vim.cmd('highlight clear SignColumn')
 vim.cmd('highlight LineNr ctermbg=NONE guibg=NONE')
+
+vim.cmd "au ColorScheme * hi Normal ctermbg=none guibg=none"
+vim.cmd "au ColorScheme * hi SignColumn ctermbg=none guibg=none"
+vim.cmd "au ColorScheme * hi NormalNC ctermbg=none guibg=none"
+vim.cmd "au ColorScheme * hi MsgArea ctermbg=none guibg=none"
+vim.cmd "au ColorScheme * hi NvimTreeNormal ctermbg=none guibg=none"
+vim.cmd "let &fcs='eob: '"
