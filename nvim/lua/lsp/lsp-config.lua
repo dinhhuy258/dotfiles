9daf06e1bca6ea24551339142f29ffffe31d17e9
Huy Duong <huy.duongdinh@gmail.com>
2021-08-04 22:19:04 +0700

    Disable lsp document highlight

--- nvim/lua/lsp/lsp-config.lua
+++ nvim/lua/lsp/lsp-config.lua
@@ -33,11 +33,6 @@ local function common_on_attach(client, _)
       hi LspReferenceRead cterm=bold ctermbg=red guibg=#464646
       hi LspReferenceText cterm=bold ctermbg=red guibg=#464646
       hi LspReferenceWrite cterm=bold ctermbg=red guibg=#464646
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
     ]],
       false
     )
