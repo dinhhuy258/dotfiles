12d2b30b31816a6b3786110a4dcda0ebae2ee7ba
Huy Duong <huy.duongdinh@gmail.com>
2021-08-05 22:21:34 +0700

    Color for lsp diagnostic signs

--- nvim/lua/iceberg/lsp.lua
+++ nvim/lua/iceberg/lsp.lua
@@ -3,6 +3,16 @@ local gitsigns = {
   LspDiagnosticsUnderlineHint = { fg = "#b4be82", style = "underline" },
   LspDiagnosticsUnderlineWarning = { fg = "#e2a478", style = "underline" },
   LspDiagnosticsUnderlineError = { fg = "#e27878", style = "underline" },
+
+  LspDiagnosticsDefaultInformation = { fg = "#84a0c6" },
+  LspDiagnosticsDefaultHint = { fg = "#b4be82" },
+  LspDiagnosticsDefaultWarning = { fg = "#e2a478" },
+  LspDiagnosticsDefaultError = { fg = "#e27878" },
+
+  LspDiagnosticsSignInformation = { fg = "#84a0c6" },
+  LspDiagnosticsSignHint = { fg = "#b4be82" },
+  LspDiagnosticsSignWarning = { fg = "#e2a478" },
+  LspDiagnosticsSignError = { fg = "#e27878" },
 }
 
 return gitsigns
