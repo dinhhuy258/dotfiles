739dfc79e578da185173aa91e33a3fd30d3fec16
Huy Duong <huy.duongdinh@gmail.com>
2021-08-09 14:57:54 +0700

    Python lsp

---
+++ nvim/lua/lsp/lang/python.lua
@@ -0,0 +1,27 @@
+local M = {}
+
+function M.config(common_on_attach, common_capabilities, common_on_init)
+  lsp_clients["python"] = {
+    formatters = {
+      {
+        -- @usage can be black or yapf or isort
+        exe = "",
+        args = {},
+      },
+    },
+    lsp = {
+      provider = "pyright",
+      setup = {
+        cmd = {
+          vim.fn.stdpath "data" .. "/lspinstall/python/node_modules/.bin/pyright-langserver",
+          "--stdio",
+        },
+        on_attach = common_on_attach,
+        on_init = common_on_init,
+        capabilities = common_capabilities,
+      },
+    },
+  }
+end
+
+return M
--- nvim/lua/lsp/lsp-clients.lua
+++ nvim/lua/lsp/lsp-clients.lua
@@ -8,6 +8,7 @@ function M.setup(common_on_attach, capabilities, common_on_init)
   require("lsp.lang.sql").config(common_on_attach, capabilities, common_on_init)
   require("lsp.lang.cpp").config(common_on_attach, capabilities, common_on_init)
   require("lsp.lang.javascript").config(common_on_attach, capabilities, common_on_init)
+  require("lsp.lang.python").config(common_on_attach, capabilities, common_on_init)
 end
 
 return M
