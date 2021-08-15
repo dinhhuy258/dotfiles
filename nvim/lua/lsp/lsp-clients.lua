a655922bef597e90855f548366bad0e4dfd6128b
Huy Duong <huy.duongdinh@gmail.com>
2021-08-15 12:27:23 +0700

    Add kotlin lsp

--- nvim/lua/lsp/lsp-clients.lua
+++ nvim/lua/lsp/lsp-clients.lua
@@ -9,6 +9,7 @@ function M.setup(common_on_attach, capabilities, common_on_init)
   require("lsp.lang.cpp").config(common_on_attach, capabilities, common_on_init)
   require("lsp.lang.javascript").config(common_on_attach, capabilities, common_on_init)
   require("lsp.lang.python").config(common_on_attach, capabilities, common_on_init)
+  require("lsp.lang.kotlin").config(common_on_attach, capabilities, common_on_init)
 end
 
 return M
