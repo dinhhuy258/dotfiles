842627b332bee369d50fdd2d5a2e5d7e15c5d59a
Huy Duong <huy.duongdinh@gmail.com>
2021-08-22 17:38:15 +0700

    Remove unused stuffs

--- nvim/lua/plugins/nvim-compe.lua
+++ nvim/lua/plugins/nvim-compe.lua
@@ -43,50 +43,8 @@ M.setup = function()
     },
   }
 
-  local t = function(str)
-    return vim.api.nvim_replace_termcodes(str, true, true, true)
-  end
-
-  local check_back_space = function()
-    local col = vim.fn.col "." - 1
-    if col == 0 or vim.fn.getline("."):sub(col, col):match "%s" then
-      return true
-    else
-      return false
-    end
-  end
-
-  -- Use (s-)tab to:
-  --- move to prev/next item in completion menuone
-  --- jump to prev/next snippet's placeholder
-  _G.tab_complete = function()
-    if vim.fn.pumvisible() == 1 then
-      return t "<C-n>"
-    elseif vim.fn["vsnip#available"](1) == 1 then
-      return t "<Plug>(vsnip-expand-or-jump)"
-    elseif check_back_space() then
-      return t "<Tab>"
-    else
-      return vim.fn["compe#complete"]()
-    end
-  end
-
-  _G.s_tab_complete = function()
-    if vim.fn.pumvisible() == 1 then
-      return t "<C-p>"
-    elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-      return t "<Plug>(vsnip-jump-prev)"
-    else
-      -- If <S-Tab> is not working in your terminal, change it to <C-h>
-      return t "<S-Tab>"
-    end
-  end
-
-  utils.set_keymap("i", "<Tab>", "v:lua.tab_complete()", { expr = true })
-  utils.set_keymap("s", "<Tab>", "v:lua.tab_complete()", { expr = true })
-  utils.set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", { expr = true })
-  utils.set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", { expr = true })
-
+  utils.set_keymap("i", "<Tab>", 'pumvisible() ? "<C-n>" : "<Tab>"', { silent = true, noremap = true, expr = true })
+  utils.set_keymap("i", "<S-Tab>", 'pumvisible() ? "<C-p>" : "<S-Tab>"', { silent = true, noremap = true, expr = true })
   utils.set_keymap("i", "<C-Space>", "compe#complete()", { noremap = true, silent = true, expr = true })
   -- utils.set_keymap("i", "<CR>", "compe#confirm('<CR>')", { noremap = true, silent = true, expr = true })
   utils.set_keymap("i", "<C-e>", "compe#close('<C-e>')", { noremap = true, silent = true, expr = true })
