51ea6e4f562a22ecdde63b434448c0464d6736e3
Huy Duong <huy.duongdinh@gmail.com>
2021-08-30 22:33:27 +0700

    Remove unused config

--- nvim/lua/plugins/nvim-autopairs.lua
+++ nvim/lua/plugins/nvim-autopairs.lua
@@ -9,22 +9,6 @@ M.setup = function()
   local npairs = require "nvim-autopairs"
   local Rule = require "nvim-autopairs.rule"
 
-  _G.MUtils = {}
-
-  vim.g.completion_confirm_key = ""
-
-  MUtils.completion_confirm = function()
-    if vim.fn.pumvisible() ~= 0 then
-      if vim.fn.complete_info()["selected"] ~= -1 then
-        return vim.fn["compe#confirm"](npairs.esc "<cr>")
-      else
-        return npairs.esc "<cr>"
-      end
-    else
-      return npairs.autopairs_cr()
-    end
-  end
-
   if package.loaded["compe"] then
     require("nvim-autopairs.completion.compe").setup {
       map_cr = true, --  map <CR> on insert mode
