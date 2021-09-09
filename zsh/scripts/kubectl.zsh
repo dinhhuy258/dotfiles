6ba7bcea1fce8e5f434a15c820523f9026e8b051
Huy Duong <huy.duongdinh@gmail.com>
2021-09-09 10:17:43 +0700

    Add more function for k8s

--- zsh/scripts/kubectl.zsh
+++ zsh/scripts/kubectl.zsh
@@ -9,10 +9,29 @@ __pick_container() {
   else
     container=$containers
   fi
+
   echo $container
 }
 
-kdel() {
+function kdes() {
+  if [ -z $1 ]; then
+    echo "Please provide resource"
+    return
+  fi
+
+  resource=$(kubectl get $1 --no-headers -o custom-columns=":metadata.name" | __fzfp)
+
+  if [ -n "$resource" ]; then
+    kubectl describe $1 ${resource}
+  fi
+}
+
+function kdel() {
+  if [ -z $1 ]; then
+    echo "Please provide resource"
+    return
+  fi
+
   resource=$(kubectl get $1 --no-headers -o custom-columns=":metadata.name" | __fzfp)
 
   if [ -n "$resource" ]; then
@@ -21,10 +40,19 @@ kdel() {
 }
 
 function klf() {
+  if [ -z $1 ]; then
+    echo "Please provide resource"
+    return
+  fi
+
   resource=$(kubectl get $1 --no-headers -o custom-columns=":metadata.name" | __fzfp)
+
   if [ -n "$resource" ]; then
     container=$(__pick_container $resource)
-    kubectl logs -f ${resource} -c $container
+
+    if [ -n "$container" ]; then
+      kubectl logs -f ${resource} -c $container
+    fi
   fi
 }
 
@@ -37,7 +65,9 @@ function kpf() {
 
   pod=$(kubectl get pods --no-headers -o custom-columns=":metadata.name" | __fzfp)
 
-  kubectl port-forward ${pod} $1
+  if [ -n "$pod" ]; then
+    kubectl port-forward ${pod} $1
+  fi
 }
 
 function ksf() {
@@ -48,13 +78,23 @@ function ksf() {
 
   svc=$(kubectl get svc --no-headers -o custom-columns=":metadata.name" | __fzfp)
 
-  kubectl port-forward svc/${svc} $1
+  if [ -n "$svc" ]; then
+    kubectl port-forward svc/${svc} $1
+  fi
 }
 
 function kexe() {
   pod=$(kubectl get pods --no-headers -o custom-columns=":metadata.name" | __fzfp)
 
+  if [ -z "$pod" ]; then
+    return
+  fi
+
   container=$(__pick_container $pod)
+
+  if [ -z "$container" ]; then
+    return
+  fi
 
   kubectl exec -it $pod -c $container -- ${1:-"sh"}
 }
