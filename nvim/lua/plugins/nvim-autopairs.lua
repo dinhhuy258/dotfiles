5bb3d116b505aa15b61dc681ae2145218739d31d
Huy Duong <huy.duongdinh@gmail.com>
2021-11-10 09:49:35 +0700

    Update nvim-autopair config

--- nvim/lua/plugins/nvim-autopairs.lua
+++ nvim/lua/plugins/nvim-autopairs.lua
@@ -1,37 +1,53 @@
 local M = {}
 
 M.setup = function()
-  local status_ok, _ = pcall(require, "nvim-autopairs")
-  if not status_ok then
-    return
-  end
-
-  local npairs = require "nvim-autopairs"
+  local autopairs = require "nvim-autopairs"
   local Rule = require "nvim-autopairs.rule"
+  local cond = require "nvim-autopairs.conds"
 
-  if package.loaded["cmp"] then
-    require("nvim-autopairs.completion.cmp").setup {
-      map_cr = true, --  map <CR> on insert mode
-      map_complete = true, -- it will auto insert `(` after select function or method item
-      auto_select = true, -- automatically select the first item
-    }
-  end
-
-  npairs.setup {
+  autopairs.setup {
     check_ts = true,
     ts_config = {
-      lua = { "string" }, -- it will not add pair on that treesitter node
+      lua = { "string" }, -- it will not add a pair on that treesitter node
       javascript = { "template_string" },
       java = false, -- don't check treesitter on java
     },
   }
 
-  require("nvim-treesitter.configs").setup { autopairs = { enable = true } }
+  autopairs.add_rule(Rule("$$", "$$", "tex"))
+  autopairs.add_rules {
+    Rule("$", "$", { "tex", "latex" }) -- don't add a pair if the next character is %
+      :with_pair(cond.not_after_regex_check "%%") -- don't add a pair if  the previous character is xxx
+      :with_pair(cond.not_before_regex_check("xxx", 3)) -- don't move right when repeat character
+      :with_move(cond.none()) -- don't delete if the next character is xx
+      :with_del(cond.not_after_regex_check "xx") -- disable  add newline when press <cr>
+      :with_cr(cond.none()),
+  }
+  autopairs.add_rules {
+    Rule("$$", "$$", "tex"):with_pair(function(opts)
+      print(vim.inspect(opts))
+      if opts.line == "aa $$" then
+        -- don't add pair on that line
+        return false
+      end
+    end),
+  }
 
+  -- If you want insert `(` after select function or method item
+  local cmp = require "cmp"
+  local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+  local map_char = {
+    all = "(",
+    tex = "{",
+  }
+  cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = map_char })
+
+  require("nvim-treesitter.configs").setup { autopairs = { enable = true } }
   local ts_conds = require "nvim-autopairs.ts-conds"
 
+  -- TODO: can these rules be safely added from "config.lua" ?
   -- press % => %% is only inside comment or string
-  npairs.add_rules {
+  autopairs.add_rules {
     Rule("%", "%", "lua"):with_pair(ts_conds.is_ts_node { "string", "comment" }),
     Rule("$", "$", "lua"):with_pair(ts_conds.is_not_ts_node { "function" }),
   }
