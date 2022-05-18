c039f767dd0b356a43f984d9c5b07a0dd787f549
Huy Duong <huy.duongdinh@gmail.com>
2022-05-19 00:19:06 +0700

    Update lsp config

--- nvim/lua/lsp/handlers.lua
+++ nvim/lua/lsp/handlers.lua
@@ -133,12 +133,12 @@ function M.setup()
   end
 
   vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-    border = "single",
+    border = "rounded",
   })
 
-  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-    border = "single",
-  })
+  -- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+  --   border = "rounded",
+  -- })
 
   vim.lsp.protocol.CompletionItemKind = {
     "   (Text) ",
