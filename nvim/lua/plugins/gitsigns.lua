fc7387ded7a457b6ae144607624c8d4aff3f6c2c
Huy Duong <huy.duongdinh@gmail.com>
2022-11-13 11:56:51 +0700

    Update gitsigns config

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -53,10 +53,36 @@ M.setup = function()
       ["n ghv"] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
       ["n ghl"] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
     },
-    watch_index = { interval = 1000 },
-    sign_priority = 6,
-    update_debounce = 200,
-    status_formatter = nil, -- Use default
+    signcolumn = true,
+      word_diff = false,
+      attach_to_untracked = true,
+      current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        delay = 1000,
+        ignore_whitespace = false,
+      },
+      current_line_blame_formatter_opts = {
+        relative_time = false,
+      },
+      max_file_length = 40000,
+      preview_config = {
+        -- Options passed to nvim_open_win
+        border = "rounded",
+        style = "minimal",
+        relative = "cursor",
+        row = 0,
+        col = 1,
+      },
+      watch_gitdir = {
+        interval = 1000,
+        follow_files = true,
+      },
+      sign_priority = 6,
+      update_debounce = 200,
+      status_formatter = nil, -- Use default
+      yadm = { enable = false },
   }
 end
 
