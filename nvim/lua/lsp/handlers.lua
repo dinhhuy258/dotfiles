2cced8a8c18e3d779a4e415d9e9348ed61b199bc
Huy Duong <huy.duongdinh@gmail.com>
2022-12-30 23:19:22 +0700

    Update lsp handler

--- nvim/lua/lsp/handlers.lua
+++ nvim/lua/lsp/handlers.lua
@@ -80,49 +80,36 @@ function M.common_on_attach(_, bufnr)
 end
 
 function M.setup()
-  local config = {
-    virtual_text = {
-      prefix = "",
-      spacing = 0,
-    },
-    signs = true,
-    underline = true,
-    update_in_insert = false,
-    severity_sort = true,
+  local signs = {
+    { name = "DiagnosticSignError", text = "" },
+    { name = "DiagnosticSignWarn", text = "" },
+    { name = "DiagnosticSignHint", text = "" },
+    { name = "DiagnosticSignInfo", text = "" },
   }
 
-  vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "LspDiagnosticsSignError" })
-  vim.fn.sign_define("DiagnosticSignWarning", { text = "", texthl = "LspDiagnosticsSignWarning" })
-  vim.fn.sign_define("DiagnosticSignInformation", { text = "", texthl = "LspDiagnosticsSignInformation" })
-  vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "LspDiagnosticsSignHint" })
-
-  vim.lsp.handlers["textDocument/publishDiagnostics"] = function(_, result, ctx, _)
-    local uri = result.uri
-    local bufnr = vim.uri_to_bufnr(uri)
-    if not bufnr then
-      return
-    end
-
-    local vim_diag = require "vim.diagnostic"
-
-    local diagnostics = result.diagnostics
-    for i, diagnostic in ipairs(diagnostics) do
-      local rng = diagnostic.range
-      diagnostics[i].lnum = rng["start"].line
-      diagnostics[i].end_lnum = rng["end"].line
-      diagnostics[i].col = rng["start"].character
-      diagnostics[i].end_col = rng["end"].character
-    end
-
-    local namespace = vim.lsp.diagnostic.get_namespace(ctx.client_id)
-    vim_diag.set(namespace, bufnr, diagnostics, config)
-
-    if not vim.api.nvim_buf_is_loaded(bufnr) then
-      return
-    end
-
-    vim_diag.show(namespace, bufnr, diagnostics, config)
+  for _, sign in ipairs(signs) do
+    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
   end
+
+  local config = {
+    virtual_text = true,
+    signs = {
+      active = signs,
+    },
+    update_in_insert = true,
+    underline = true,
+    severity_sort = true,
+    float = {
+      focusable = true,
+      style = "minimal",
+      border = "rounded",
+      source = "always",
+      header = "",
+      prefix = "",
+    },
+  }
+
+  vim.diagnostic.config(config)
 
   vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
     border = "rounded",
