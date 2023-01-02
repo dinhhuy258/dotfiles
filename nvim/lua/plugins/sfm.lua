0da186cdc1d2057ccda9c612dc019e04097149af
Huy Duong <huy.duongdinh@gmail.com>
2023-01-02 11:42:22 +0700

    Typo

--- nvim/lua/plugins/sfm.lua
+++ nvim/lua/plugins/sfm.lua
@@ -2,8 +2,8 @@ local M = {}
 
 M.setup = function()
   local sfm_explorer = require("sfm").setup {}
-  sfm_explorer:load_extention "sfm-bookmark"
-  sfm_explorer:load_extention "sfm-filter"
+  sfm_explorer:load_extension "sfm-bookmark"
+  sfm_explorer:load_extension "sfm-filter"
 
   local utils = require "utils"
   utils.set_keymap("n", "<F1>", "<CMD>SFMToggle<CR>", { noremap = true, silent = true })
