908592be5886c3f434f8022d84fcaaf94fb71011
Huy Duong <huy.duongdinh@gmail.com>
2023-09-23 10:05:00 +0700

    Delete buffer on sfm delete event

--- nvim/lua/plugins/sfm.lua
+++ nvim/lua/plugins/sfm.lua
@@ -1,4 +1,6 @@
 local keymap = require "utils.keymap"
+local bdelete = require("barbar.bbye").bdelete
+local event = require "sfm.event"
 
 local M = {}
 
@@ -39,6 +41,12 @@ M.setup = function()
   sfm_explorer:load_extension "sfm-git"
   sfm_explorer:load_extension "sfm-telescope"
   sfm_explorer:load_extension "sfm-paste"
+
+  sfm_explorer:subscribe(event.EntryDeleted, function(payload)
+    local path = payload["path"]
+    local bufnr = vim.fn.bufnr(path, true)
+    bdelete(false, bufnr)
+  end)
 
   keymap.set("n", "<F1>", "<CMD>SFMToggle<CR>", { noremap = true, silent = true })
   keymap.set("n", "fm", "<CMD>SFMToggle<CR>", { noremap = true, silent = true })
