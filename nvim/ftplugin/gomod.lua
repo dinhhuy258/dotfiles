27b3397dac2afa3b38cf19c25dc7a2f2aca0e2f4
Huy Duong <huy.duongdinh@gmail.com>
2024-02-22 19:34:50 +0700

    nvim ftplugin

---
+++ nvim/ftplugin/gomod.lua
@@ -0,0 +1,3 @@
+vim.opt_local.expandtab = false -- Use tabs instead of spaces
+vim.opt_local.softtabstop = 4 -- Number of spaces that a <Tab> in the file counts for
+vim.opt_local.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent. Used for >>, <<, etc
---
+++ nvim/ftplugin/java.lua
@@ -0,0 +1,3 @@
+vim.opt_local.expandtab = true -- Convert tabs to spaces
+vim.opt_local.softtabstop = 4 -- Number of spaces that a <Tab> in the file counts for
+vim.opt_local.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent. Used for >>, <<, etc
