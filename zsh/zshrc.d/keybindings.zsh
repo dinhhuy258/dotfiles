1088f88159661337aa94f01733897676d39d0db5
Huy Duong <huy.duongdinh@gmail.com>
2024-03-12 23:05:59 +0700

    fzf-tab plugin

--- zsh/zshrc.d/keybindings.zsh
+++ zsh/zshrc.d/keybindings.zsh
@@ -7,11 +7,10 @@ bindkey '^v' edit-command-line
 zle -N fprj
 bindkey '^ ' fprj
 
-zle -N fpass
-bindkey '^p' fpass
-
 # see `man bash` to define more key bindings
 bindkey "^[[1;3C" forward-word
 bindkey "^[[1;3D" backward-word
 bindkey "^i" kill-line
 bindkey "^y" kill-whole-line
+
+bindkey '\t' complete-word
