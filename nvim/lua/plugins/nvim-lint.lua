bdc3ba993c740dfd98ebc74507561b3c76e6b9d6
Huy Duong <huy.duongdinh@gmail.com>
2024-07-10 21:59:27 +0700

    pylint not work well on venv

--- nvim/lua/plugins/nvim-lint.lua
+++ nvim/lua/plugins/nvim-lint.lua
@@ -11,7 +11,7 @@ M.setup = function()
     typescript = { "eslint_d" },
     javascriptreact = { "eslint_d" },
     typescriptreact = { "eslint_d" },
-    python = { "pylint" },
+    python = { "ruff" },
     go = { "golangcilint" },
     ruby = { "rubocop" },
   }
