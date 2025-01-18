63eb7dcd59f7920c8e0081e922c0575e62221d95
Huy Duong <huy.duongdinh@gmail.com>
2025-01-18 13:57:54 +0700

    sketchybar: Update wifi script

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SSID="$(networksetup -getairportnetwork en0 | awk -F ': ' '{print $2}')"
+SSID="$(ipconfig getsummary en0 | awk -F ' SSID : '  '/ SSID : / {print $2}')"
 
 if [ "$SSID" = "" ]; then
   sketchybar --set "$NAME" label="Disconnected" icon=󰖪
