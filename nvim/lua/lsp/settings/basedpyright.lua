1789392f25f92c3b0f84790df57b89eb82951917
Huy Duong <huy.duongdinh@gmail.com>
2025-05-16 10:34:12 +0700

    refactor(lsp): simplify basedpyright type checking config

--- nvim/lua/lsp/settings/basedpyright.lua
+++ nvim/lua/lsp/settings/basedpyright.lua
@@ -2,18 +2,7 @@ return {
   settings = {
     basedpyright = {
       analysis = {
-        diagnosticSeverityOverrides = {
-          reportUnknownArgumentType = false,
-          reportUnknownMemberType = false,
-          reportUnknownVariableType = false,
-          reportUnknownParameterType = false,
-          reportMissingParameterType = false,
-          reportMissingTypeArgument = false,
-          reportMissingTypeStubs = false,
-          reportUntypedFunctionDecorator = false,
-          reportImplicitRelativeImport = false,
-          reportAny = false,
-        },
+        typeCheckingMode = "basic",
       },
     },
   },
