f006be8704f1ac8653471ec74df5ee735b62f655
Huy Duong <huy.duongdinh@gmail.com>
2025-12-07 14:16:19 +0700

    Revert "feat(nvim): use fzf.nvim"
    This reverts commit 636724af2b1714c95eee044e1c2d7d14fdf25d82.

---
+++ nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,26 @@
+local keymaps = require "config.keymaps"
+
+local M = {}
+
+M.setup = function()
+  local fzf_status_ok, fzf = pcall(require, "fzf-lua")
+  if not fzf_status_ok then
+    return
+  end
+
+  local actions = require "fzf-lua.actions"
+
+  fzf.setup {
+    actions = {
+      files = {
+        true,
+        ["ctrl-h"] = actions.file_split,
+        ["ctrl-v"] = actions.file_vsplit,
+      },
+    },
+  }
+
+  keymaps.set("n", "<Leader>ff", ":lua require('fzf-lua').files()<CR>", { noremap = true })
+end
+
+return M
