ff3e8ceda96b732e7d73b72028cc54aac7b11606
Huy Duong <huy.duongdinh@gmail.com>
2026-03-17 22:09:19 +0700

    feat(navi): gh cheatsheet

--- zsh/zshrc.d/navi-shortcuts.zsh
+++ zsh/zshrc.d/navi-shortcuts.zsh
@@ -1,2 +1,5 @@
 # Docker shortcuts
 ddel() { navi --best-match --tag-rules "docker" --query "docker stop"; }
+
+# GitHub shortcuts
+apr() { pr_url="$1" navi --best-match --tag-rules "gh" --query "Approve a pull request"; }
