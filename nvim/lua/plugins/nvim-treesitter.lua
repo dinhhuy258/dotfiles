284ce3962d9e8de1464d68d7bfd4ae68357c3d57
Huy Duong <huy.duongdinh@gmail.com>
2026-04-10 09:07:21 +0700

    chore(nvim): treesitter ensure install

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,38 @@
 local M = {}
 
 M.setup = function()
+  local ok, ts = pcall(require, "nvim-treesitter")
+  if not ok then
+    return
+  end
+
+  ts.install {
+    "bash",
+    "c",
+    "cpp",
+    "css",
+    "dockerfile",
+    "go",
+    "html",
+    "java",
+    "javascript",
+    "json",
+    "kotlin",
+    "lua",
+    "markdown",
+    "php",
+    "python",
+    "ruby",
+    "rust",
+    "sql",
+    "swift",
+    "toml",
+    "typescript",
+    "vim",
+    "vimdoc",
+    "yaml",
+  }
+
   vim.api.nvim_create_autocmd("FileType", {
     callback = function()
       if vim.treesitter.get_parser(0, nil, { error = false }) then
